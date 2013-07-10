0ef67958f5645831182d4e383a14b920f1202e23
Pedro M. Lledo <pmlledo@gmail.com>
2013-07-10 23:00:50 +0200

    Updated grader.js with URL option

--- grader.js
+++ grader.js
@@ -21,11 +21,35 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
-var fs = require('fs');
+var fs      = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest    = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
+};
+
+
+var checkUrl = function (url, checksfile) {
+    var checks = loadChecks(checksfile).sort();
+    rest.get(program.url).on('complete', function(data) {
+        var out = {};
+        $ = cheerio.load(data);
+        for (var ii in checks) {
+            var present = $(checks[ii]).length > 0;
+            out[checks[ii]] = present;
+        }
+        console.log(out);
+        return out;
+    });
+};
+
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +89,16 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <URL>', 'URL to index.html' )
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+		if (program.url && program.checks) {
+        var checkJson = checkUrl(program.url, program.checks);
+		} else if (program.file && program.checks) {
+		    var checkJson = checkHtmlFile(program.file, program.checks);
+		    var outJson = JSON.stringify(checkJson, null, 4);
+	  } else {
+        console.log('Some parameters missing!');
+	  }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
